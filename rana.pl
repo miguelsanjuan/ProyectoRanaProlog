c61909efe0e4ab4743d521139f2b75612e145539
miguelsanjuan <miguelsanjuanva@gmail.com>
2019-11-11 13:07:10 -0700

    segunda Version Rana

--- rana.pl
+++ rana.pl
@@ -1,48 +1,47 @@
-%Elabore los predicados necesarios para imprimir la canción “Estaba la
-% rana sentada &debajo
-% del agua”, usando listas para definir a cada uno de los personajes que
-% intervienen en esta,
-% debe estar registrados sólo como [ rana, mosca, araña …] sin el
-% artículo que le corresponde,
-% según el género del ítem nombrado. Este debe ser parte de lo que se
-% agrega al usar la
-% recursividad. Sólo puede haber dos mayúsculas, la del primer personaje
-% de cada estrofa y
-%usada en la línea “Cuando XX se puso a cantar”
-
 %base de conocimientos
-personajes([rana,mosca,araña,rata,gato,perro,hombre,abuela,nieta]).
+personajes([rana,mosca,arana,rata,gato,perro,hombre,abuela,nieta]).
 pronombre(rana,la).
-pronombre(araña,la).
+pronombre(arana,la).
 pronombre(rata,la).
 pronombre(mosca,la).
 pronombre(abuela,la).
-pronombre(nieta,la).
+pronombre(nieta,su).
 pronombre(gato,el).
 pronombre(hombre,el).
 pronombre(perro,el).
+
+%ver si se puede hacer la lista de order sin hechos
+
 siguiente(rana,mosca).
-siguiente(mosca,araña).
-siguiente(araña,rata).
+siguiente(mosca,arana).
+siguiente(arana,rata).
 siguiente(rata,gato).
 siguiente(gato,perro).
 siguiente(perro,hombre).
 siguiente(hombre,abuela).
 siguiente(abuela,nieta).
+%cambiar esto
+%predicado fijo
 sentadabjoAgua(rana).
+
+
+%simbologia 
+%pre:
+%ps:
+%pro:
+
 %inicio de la cancion.
 inicio:-personajes([X|Y]),pronombre(X,Pre),siguiente(X,PS),pronombre(PS,Pro),
-write("Estaba "),write(Pre),write(" "),write(X),write("  sentada de bajo del agua"),
-nl,write("Cuando "),write(Pre),write(" "),write(X),write(" se puso a cantar "),nl,
-write(" vino "),write(" "),write(Pro),write(" "),write(PS),write(" y "),write(Pre), write(" hizo callar"),
-write(Y),nl,cancion(Y).
+write("Estaba "),write(Pre),write(" "),write(X),write("  sentada cantando de bajo del agua"),
+nl,write("Cuando "),write(Pre),write(" "),write(X),write(" "),write(" se puso a cantar "),nl,
+write(" vino "),write(" "),write(Pro),write(" "),write(PS),write(" y "),write(Pre), write(" hizo callar"),nl,cancion(Y).
 %reglaas que recorren la lista y escriben la cancion
-cancion([X|Y]):-not(X=nieta),nl,quienes(X),nl,sentadabjoAgua(R),write(""),pronombre(R,P),write(P),
-write(" "),write(R),write(" que estaba sentada debajo del agua"),nl,
-write("Cuando"),pronombre(X,Pre),write(" "),write(Pre),write(" "),write(X),write("se puso a cantar"),nl,
+cancion([X|Y]):-not(X=nieta),nl,quienes(X),nl,write(" la rana que estaba cantando sentada debajo del agua"),nl,
+write("Cuando"),pronombre(X,Pre),write(" "),write(Pre),write(" "),write(X),write(" "),write("se puso a cantar"),nl,
 write("vino "),siguiente(X,PS),write(" "),pronombre(PS,Pro),
-write(Pro),write(" "),write(PS),write(" y "),write(Pre), write(" hizo callar"),write(Y),nl,cancion(Y).
-cancion([X|[]]):-nl,write("Tan, Tan").
+write(Pro),write(" "),write(PS),write(" y "),write(Pre), write(" hizo callar"),nl,cancion(Y).
+%rompe el ciclo y hace que termine la cancion
+cancion([X|[]]):-nl,write("Fin").
 %quien va de quien
 quienes(Siguiente):-siguiente(Anterior,Siguiente),
           pronombre(Anterior,PronAnt),
@@ -50,87 +49,6 @@ quienes(Siguiente):-siguiente(Anterior,Siguiente),
           write(PronSig),write(" "),write(Siguiente),write(" a "),write(PronAnt),
           write(" "),write(Anterior),write(","),nl,
           (   quienes(Anterior);true).
-
-
-
-          %estaba(X):-personajes([X|[T|_]]),personajes([X|P]),write("Estaba "),write(" "),prefijo(X,Y),write(Y),
-         % write(" "),sentadabjoAgua(R), write(R),write(" sentada debajo del agua"),
-         %nl,write(" Cuando "),write(Y),write(" "),write(X),write(" se puso a cantar"),
-         %write(" vino "),prefijo(T,M),write(M),write(" "),write(T),write(" y"),write(" "),
-         %write(" hizo callar"),
-		 %nl,
-		 %retract(personajes([_|_])),
-         %assert(personajes(P)),
-		 %estaba1(P).
-
-%estaba1([]):-write("se acabo la cancion").
-
-%estaba1([X|P]):-
-          %personajes([X|[T|_]]),personajes([X|P]),prefijo(X,Genero),prefijo(T,GeneroSig),
-          %write(" Cuando "),write(" "),write(Genero),write(" "),write(X),write(" se puso a cantar"),
-          %write(" vino "),write(GeneroSig),write(" "),write(T),write(" y"),write(" "),
-          %write(" hizo callar"),nl,
-		  %retract(personajes([_|_])),
-          %assert(personajes(P)),
-          %estaba1(P).
-
-
-
-% estaba(X):-not(X=rana),personajes([_|[_|[X|[T|_]]]]),write("Estaba
-% "),write(" "),write(" "),sentadabjoAgua(R),write(R),write(" sentada
-% debajo del agua"),nl,write(" Cuando "),write(" "),write(X),write(" se
-% puso a cantar"),write(" vino "),write(" "),write(" "),write(T),write("
-% y"),write(" "),write(" hizo callar"),nl,estaba(T).
-
-% estaba(X):-not(X=rana),personajes([_|[_|[_|[X|[T|_]]]]]),write("Estaba
-% "),write(" "),sentadabjoAgua(R),write(R),write(" sentada debajo del
-% agua"),nl,write(" Cuando "),write(" "),write(X),write(" se puso a
-% cantar"),write(" vino "),write(" "),write(T),write(" y"),write("
-% "),write(" hizo callar"),nl,estaba(T).
-
-% estaba(X):-not(X=rana),personajes([_|[_|[_|[_|[X|[T|_]]]]]]),write("Estaba
-% "),prefijo(R,N),write(N),write(" "),sentadabjoAgua(R),write(R),write("
-% sentada debajo del agua"),nl,write(" Cuando "),prefijo(X,Y),write("
-% "),write(Y),write(" "),write(X),write(" se puso a cantar"),write(" vino
-% "),prefijo(T,M),write(M),write(" "),write(" "),write(T),write("
-% y"),write(" "),write(" hizo callar"),nl,estaba(T).
-
-% estaba(X):-not(X=rana),personajes([_|[_|[_|[_|[_|[X|[T|_]]]]]]]),write("Estaba
-% "),write(" "),sentadabjoAgua(R),write(R),write(" sentada debajo del
-% agua"),nl,write(" Cuando "),write(" "),write(X),write(" se puso a
-% cantar"),write(" vino "),write(" "),write(T),write(" y"),write("
-% "),write(" hizo callar"),nl,estaba(T).
-
-% estaba(X):-not(X=rana),personajes([_|[_|[_|[_|[_|[_|[X|[T|_]]]]]]]]),write("Estaba
-% "),write(" "),sentadabjoAgua(R),write(R),write(" sentada debajo del
-% agua"),nl,write(" Cuando "),write(" "),write(X),write(" se puso a
-% cantar"),write(" vino "),write(" "),write(T),write(" y"),write("
-% "),write(" hizo callar"),nl,estaba(T).
-
-% estaba(X):-not(X=rana),personajes([_|[_|[_|[_|[_|[_|[_|[X|[T|_]]]]]]]]]),write("Estaba
-% "),write(" "),sentadabjoAgua(R),write(R),write(" sentada debajo del
-% agua"),nl,write(" Cuando "),write(" "),write(X),write(" se puso a
-% cantar"),write(" vino "),write(" "),write(T),write(" y"),write("
-% "),write(" hizo callar"),nl,estaba(T).
-
-% estaba(X):-not(X=rana),personajes([_|[_|[_|[_|[_|[_|[_|[_|[X|[T|_]]]]]]]]]]),write("Estaba
-% "),write(" "),sentadabjoAgua(R),write(R),write(" sentada debajo del
-% agua"),nl,write(" Cuando "),write(" "),write(X),write(" se puso a
-% cantar"),write(" vino "),write(" "),write(T),write(" y"),write("
-% "),write(" hizo callar"),nl,estaba(T).
-
-
-
-
-% estaba(X):-personajes([X|[T|_]]),write("Estaba ") , write(rana),write("
-% sentada debajo del agua"), write("Cuando "),write(X),write("Sepuso a
-% cantar"),estaba(T).
-%estaba([]).
-%
-
-
-
-
 
 
 
